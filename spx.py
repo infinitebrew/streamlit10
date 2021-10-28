67010812d54de3903b187b1283d0060efc49830a
infinitebrew <infinitebrew@gmail.com>
2021-10-28 11:34:41 -0400

    Update spx.py

--- spx.py
+++ spx.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import numpy as np
 import datetime
 
